c23355601b5e4d83ac71b129b5e562e2c6d53873
David <davidakpughe2@gmail.com>
2020-03-10 14:24:00 +0100

    adding styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Customer from './Customer';
+import Support from './Support';
 import './App.css';
+import './css/normalize.css'
+import './css/skeleton.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" render={() => <Customer />} />
+      <Route exact path="/support" render={() => <Support />} />
     </div>
   );
 }
---
+++ client/src/Customer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+
+const Customer = () => {
+    return null;
+}
+export default Customer
